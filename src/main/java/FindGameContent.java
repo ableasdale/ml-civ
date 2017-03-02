c434ad7d1cc145ae393e86038c36cd370c47ba7b
Alex Bleasdale <ableasdale@marklogic.com>
2017-03-02 19:29:07 +0000

    Initial project files

---
+++ src/main/java/FindGameContent.java
@@ -0,0 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by ableasdale on 02/03/17.
+ */
+public class FindGameContent {
+
+    private static final String STEAM_BASE_DIR = "/home/ableasdale/.local/share/Steam/steamapps/common";
+    private static final Logger LOG = LoggerFactory.getLogger("FindGameContent");
+
+    public static void main(String[] args) {
+        LOG.info("Started");
+
+    }
+}
